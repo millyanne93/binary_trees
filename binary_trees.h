1839088880b39446a76f5a97147539cea98b3074
millyanne93 <nmillyanne20@gmail.com>
2024-01-04 05:32:16 -0800

    binary_tree

--- binary_trees.h
+++ binary_trees.h
@@ -32,6 +32,17 @@ typedef struct binary_tree_s avl_t;
 /* Max Binary Heap */
 typedef struct binary_tree_s heap_t;
 
+/**
+ * struct levelorder_queue_s - Level order traversal queue.
+ * @node: A node of a binary tree.
+ * @next: The next node to traverse to in the binary tree.
+ */
+typedef struct levelorder_queue_s
+{
+	binary_tree_t *node;
+	struct levelorder_queue_s *next;
+} levelorder_queue_t;
+
 /*mandatory tasks*/
 size_t binary_tree_height(const binary_tree_t *tree);
 void binary_tree_print(const binary_tree_t *);
