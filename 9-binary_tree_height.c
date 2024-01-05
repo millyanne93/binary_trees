84741f13b34c41f84a72313bb475bcb3ad3f0b6b
millyanne93 <nmillyanne20@gmail.com>
2024-01-05 06:14:59 -0800

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,15 +6,12 @@
  * @tree: tree to go through
  * Return: the height
  */
-
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t l, r;
-
 	if (tree == NULL)
 		return (0);
 
-	l = binary_tree_height(tree->left);
-	r = binary_tree_height(tree->right);
-	return (1 + (l > r ? l : r));
+	size_t left_height = binary_tree_height(tree->left);
+	size_t right_height = binary_tree_height(tree->right);
+	return (1 + (left_height > right_height ? left_height : right_height));
 }
